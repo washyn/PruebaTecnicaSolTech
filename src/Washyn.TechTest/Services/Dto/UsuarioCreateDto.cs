c9f7f554e7621b5b1fdfa32e605974b220214634
Washington Acero M <shin321654987@gmail.com>
2024-05-06 12:06:49 -0700

    aded validations

--- src/Washyn.TechTest/Services/Dto/UsuarioCreateDto.cs
+++ src/Washyn.TechTest/Services/Dto/UsuarioCreateDto.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Washyn.TechTest.Services.Dto;
 
 public class UsuarioCreateDto
 {
+    [Required]
     public string UserName { get; set; }
+    
+    [Required]
     public string Password { get; set; }
+    
+    [DataType(DataType.EmailAddress)]
     public string Email { get; set; }
+    
+    [Required]
     public string Name { get; set; }
+    
+    [Required]
     public string Surname { get; set; }
+    
     public string? PhoneNumber { get; set; }
 }
--- src/Washyn.TechTest/Services/Dto/UsuarioUpdateDto.cs
+++ src/Washyn.TechTest/Services/Dto/UsuarioUpdateDto.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Washyn.TechTest.Services.Dto;
 
 public class UsuarioUpdateDto
 {
+    [Required]
     public string UserName { get; set; }
+    
+    [Required]
+    [DataType(DataType.EmailAddress)]
     public string Email { get; set; }
+    
+    [Required]
     public string Name { get; set; }
+    
+    [Required]
     public string Surname { get; set; }
     public string? PhoneNumber { get; set; }
 }
